5b8dae5dcedff4842857b32f7aeb11da02cb982e
ylabate <12196@holbertonstudents.com>
2025-12-08 13:15:12 +0000

    fix: Correct typo in binary_tree_postorder function documentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using post-order traversa
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  *
