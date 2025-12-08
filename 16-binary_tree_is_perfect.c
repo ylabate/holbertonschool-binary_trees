274a3a1e1f101d71ee6360a179647c8f20986a1a
ylabate <12196@holbertonstudents.com>
2025-12-08 15:28:11 +0000

    feat: Add function to check if a binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	unsigned int l_height, r_height;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	l_height = binary_tree_is_perfect(tree->left);
+	r_height = binary_tree_is_perfect(tree->right);
+
+	return ((l_height == r_height ? 1 : 0));
+}
