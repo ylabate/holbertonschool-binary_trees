47ef8bcc475e305e02de95721a1a2e876da2d547
ylabate <12196@holbertonstudents.com>
2025-12-08 10:07:44 +0000

    fix: Correct parent pointer assignment in binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 	}
 	else
-		new_node->parent = NULL;
+		free(new_node);
 
 	return (new_node);
 }
