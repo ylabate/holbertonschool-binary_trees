8004be8b95e812d0417153434dc333edb9ca456f
ylabate <12196@holbertonstudents.com>
2025-12-08 15:13:10 +0000

    feat: Add function to count the leaves in a binary tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the leaves
+ *
+ * Return: number of leaves in the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	unsigned int size = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree && tree->left)
+		size += binary_tree_leaves(tree->left);
+
+	if (tree && tree->right)
+		size += binary_tree_leaves(tree->right);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	return (size);
+}
