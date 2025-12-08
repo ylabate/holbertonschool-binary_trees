98147d26d06405cec5aeef8ed51cf1e7fdfbaea3
ylabate <12196@holbertonstudents.com>
2025-12-08 13:14:34 +0000

    feat: Add post-order traversal function for binary trees fix: Simplify pre-order traversal function logic

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,11 +10,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		func(tree->n);
-		if (tree)
-		{
-			binary_tree_preorder(tree->left, func);
-			binary_tree_preorder(tree->right, func);
-		}
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
 
