1848a58f42e17d7f0d34d4c4ed3f3f1145c50c56
ylabate <12196@holbertonstudents.com>
2025-12-09 09:40:22 +0000

    feat: Add function to find the uncle of a binary tree node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ *
+ * Return: pointer to the uncle node, or NULL if no uncle exists
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t temp;
+
+	if (node && node->parent && node->parent->parent &&
+		(node->parent->parent->left || node->parent->parent->right))
+	{
+		temp = *node->parent->parent;
+		node = node->parent;
+		if (node == temp.left)
+			return (temp.right);
+		else
+			return (temp.left);
+	}
+	return (NULL);
+}
