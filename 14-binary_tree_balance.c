5536d93cca787d29dceb9becf70a7bbbf0b73029
ylabate <12196@holbertonstudents.com>
2025-12-09 09:25:37 +0000

    feat: Implement binary_tree_height function to measure the height of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,3 +21,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (balance);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	unsigned int l_height, r_height;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+
+	return ((l_height > r_height ? l_height : r_height) + 1);
+}
