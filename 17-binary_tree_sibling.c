50c9bd177d59cb4a6bd99fa065d7befa1b9b3514
ylabate <12196@holbertonstudents.com>
2025-12-09 09:35:38 +0000

    fix: Update function documentation for binary_tree_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ *
+ * Return: pointer to the sibling node, or NULL if no sibling exists
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t temp;
